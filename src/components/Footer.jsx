f80571abcc9cf1b0f1f2dfa59e83433c5345e053
scintillating-Shruti-Pandey <shrutipandey369.1729@gmail.com>
2026-06-11 18:04:12 +0530

    footer: link creator name to portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <p>Beautifully crafted with React</p>
-      <p className="creator">Created by <strong>Shruti Pandey</strong></p>
+      <p className="creator">Created by <strong><a href="https://scintillating-shruti-pandey.github.io/Portfolio" target="_blank" rel="noopener noreferrer">Shruti Pandey</a></strong></p>
     </footer>
   );
 };
